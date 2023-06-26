f1c862afb08efabc7e579180630574cb2be1e507
Эlmirbek.A.A <107041483+elmirzens@users.noreply.github.com>
2023-06-26 16:57:30 +0600

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,14 +2,8 @@ package org.example;
 
 import org.example.repository.PersonDAO;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) {
         PersonDAO personDAO = new PersonDAO();
-
-        personDAO.deleteAll();
     }
 }
